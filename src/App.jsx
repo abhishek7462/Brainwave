f817c865733f306c3b73ddba0896b0f9e1d5db4d
abhishek7462 <abhi2198m@gmail.com>
2025-01-14 12:14:09 +0530

    improve responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header.jsx";
 import Button from "./components/Button/Button";
 
 import "./index.css";
-import Benefits from "./components/Benefits/Benefits";
+import Benefits from "./components/Benefits/Benefits.jsx";
 // import GenerativeAI from "./components/GenerativeAI";
-import GenerativeSection from "./components/GenerativeSection/GenerativeSection";
-import Pricing from "./components/Pricing/Pricing";
-import Roadmap from "./components/Roadmap/Roadmap";
-import Hero from "./components/Hero/Hero";
-import Footer from "./components/Footer/Footer";
+import GenerativeSection from "./components/GenerativeSection/GenerativeSection.jsx";
+import Pricing from "./components/Pricing/Pricing.jsx";
+import Roadmap from "./components/Roadmap/Roadmap.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 const App = () => {
   return (
     <>
-      <div className="container">
-        <Header />
-        <Hero />
-        <Benefits />
-        <GenerativeSection />
-        <Pricing />
-        <Roadmap />
-        <Footer />
-      </div>
+      {/* <div className="container"> */}
+      <Header />
+      <Hero />
+      <Benefits />
+      <GenerativeSection />
+      <Pricing />
+      <Roadmap />
+      <Footer />
+      {/* </div> */}
     </>
   );
 };
